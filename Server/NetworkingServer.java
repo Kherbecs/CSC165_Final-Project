2682a0e0373cc4545d75c7d88553b9d3ffab5052
yandereindica <bbdoney@gmail.com>
2023-05-11 18:38:03 -0700

    Adding support to AI. Still adding towards.

--- Server/NetworkingServer.java
+++ Server/NetworkingServer.java
@@ -1,29 +1,52 @@
+package tage;
+
 import java.io.IOException;
 import tage.networking.IGameConnection.ProtocolType;
+import tage.networking.*;
 
 public class NetworkingServer 
 {
 	private GameServerUDP thisUDPServer;
 	private GameServerTCP thisTCPServer;
+	private GameAIServerUDP UDPServer;
+	private NPCcontroller npcCtrl;
 
 	public NetworkingServer(int serverPort, String protocol) 
-	{	try 
-		{	if(protocol.toUpperCase().compareTo("TCP") == 0)
-			{	thisTCPServer = new GameServerTCP(serverPort);
+	{
+		npcCtrl = new NPCcontroller();
+		// start networking server
+		try 
+		{	
+			if(protocol.toUpperCase().compareTo("TCP") == 0)
+			{	
+				thisTCPServer = new GameServerTCP(serverPort);
+			} else {	
+				thisUDPServer = new GameServerUDP(serverPort);
 			}
-			else
-			{	thisUDPServer = new GameServerUDP(serverPort);
-			}
-		} 
-		catch (IOException e) 
-		{	e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+		try
+		{ 
+			UDPServer = new GameAIServerUDP(serverPort, npcCtrl);
+
+		} catch (IOException e) { 
+			System.out.println("server didn't start"); e.printStackTrace();
+		}
+			npcCtrl.start(UDPServer);
 	}
 
 	public static void main(String[] args) 
-	{	if(args.length > 1)
-		{	NetworkingServer app = new NetworkingServer(Integer.parseInt(args[0]), args[1]);
+	{
+		if(args.length > 1)
+		{	
+			NetworkingServer app = new NetworkingServer(Integer.parseInt(args[0]), args[1]);
+		}
+
+		if(args.length == 1)
+		{ 
+			NetworkingServer app = new NetworkingServer(Integer.parseInt(args[0]), args[1]);
 		}
 	}
-
 }
